f3e3e82684e24eb176164ce382cfd8778cc52d67
bouzuya <m@bouzuya.net>
2016-10-16 19:59:45 +0900

    add whatwg-streams

---
+++ whatwg-streams/index.js
@@ -0,0 +1,3 @@
+module.exports.ReadableStream = require('./lib/readable-stream').ReadableStream;
+module.exports.TransformStream = require('./lib/transform-stream');
+module.exports.WritableStream = require('./lib/writable-stream').WritableStream;
