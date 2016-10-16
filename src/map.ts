360c45fc0b14f31b1a843fbda312b5aac2947870
bouzuya <m@bouzuya.net>
2016-10-16 22:00:33 +0900

    fix transform signature

--- src/map.ts
+++ src/map.ts
@@ -2,9 +2,8 @@ import { TransformStream } from 'whatwg-streams-b';
 
 const map = (f: (i: any) => any): TransformStream => {
   return new TransformStream({
-    transform(chunk: any, done: () => any, enqueue: (chunk: any) => any): any {
-      enqueue(f(chunk));
-      done();
+    transform(chunk, controller) {
+      controller.enqueue(f(chunk));
     }
   });
 };
