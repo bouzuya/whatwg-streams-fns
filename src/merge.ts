060c3c86ed81b7193eb8faaa4d4526261472d4fe
bouzuya <m@bouzuya.net>
2016-10-18 19:26:08 +0900

    update merge() for rest parameters support

--- src/merge.ts
+++ src/merge.ts
@@ -1,32 +1,25 @@
 import { ReadableStream, WritableStream } from 'whatwg-streams-b';
 
-const merge = (rs1: ReadableStream, rs2: ReadableStream): ReadableStream => {
+const merge = (...readables: ReadableStream[]): ReadableStream => {
   // TODO: options
   const options = void 0;
   return new ReadableStream({
     cancel(reason) {
-      return Promise.all([
-        rs1.cancel(reason),
-        rs2.cancel(reason)
-      ]);
+      return Promise.all(readables.map((readable) => readable.cancel(reason)));
     },
     start(controller) {
-      void Promise.all([
-        rs1.pipeTo(new WritableStream({
+      void Promise.all(readables.map((readable) => {
+        return readable.pipeTo(new WritableStream({
           write(chunk) {
             controller.enqueue(chunk);
           }
-        })),
-        rs2.pipeTo(new WritableStream({
-          write(chunk) {
-            controller.enqueue(chunk);
-          }
-        }))
-      ]).then(() => {
-        controller.close();
-      }, (error) => {
-        controller.error(error);
-      });
+        }));
+      }))
+        .then(() => {
+          controller.close();
+        }, (error) => {
+          controller.error(error);
+        });
     }
   }, options);
 };
